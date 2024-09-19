c6841c76ee81141654ec9ac1184cd18e0b492215
Мирзошарифова Аделия Фирузовна <352315@mck-ktits.ru>
2024-09-19 11:13:37 +0300

    commit

---
+++ sm2/main.js
@@ -0,0 +1,21 @@
+let name = prompt("Как тебя зовут?");
+
+alert (`Тебя зовут ${name}`);
+
+let age = prompt ("Сколько тебе лет?");
+
+if (age < 20) 
+{
+    alert (` Тебе меньше 20`) 
+}
+ else { 
+    for (let i = 0; i < 5; i++)
+    {
+        alert ("Привет!")
+    }
+} 
+
+if (confirm("Хочешь учить js?")) {
+   
+      alert("Супер!")
+}
